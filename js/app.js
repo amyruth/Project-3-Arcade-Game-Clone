88a60ebfb8428fe0efb5254e93acb6d2d16259a2
amyruth <anrutherford@gmail.com>
2018-07-03 18:08:54 -0400

    added collision detection

--- js/app.js
+++ js/app.js
@@ -7,9 +7,39 @@ function randomSpeed(min, max) {
 const randomRow = function() {
  return [60, 140, 225][Math.floor(Math.random() * [60, 140, 225].length)];
 };
-	
+
+function playerReset() {
+	player.x = 200;
+	player.y = 400;
+}
+
+function checkCollisions(){
+		if(player.x + 10 > enemy1.x - 10 && player.x - 10< enemy1.x + 10 &&
+		player.y + 10 > enemy1.y - 10 &&
+		player.y - 10 < enemy1.y + 10) {
+			console.log('hit 1');
+		playerReset();
+	}
+
+	else if(player.x + 10 > enemy2.x - 10 && 
+		player.x - 10 < enemy2.x + 10 &&
+		player.y + 10 > enemy2.y - 10 &&
+		player.y - 10 < enemy2.y + 10) {
+		playerReset();
+	}
+
+	else if(player.x + 10 > enemy3.x - 10 && 
+		player.x -10 < enemy3.x + 10 &&
+		player.y + 10 > enemy3.y - 10 &&
+		player.y - 10 < enemy3.y + 10) {
+		playerReset();
+	}
+}
+
+
+
 // Enemies our player must avoid
-let Enemy = function(x) {
+let Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -18,23 +48,18 @@ let Enemy = function(x) {
 	this.sprite = 'images/enemy-bug.png';
 	this.x = -100;
 	this.y = randomRow();
-	this.speed = randomSpeed(2, 8);
+	this.speed = randomSpeed(2, 5);
 };
+
+const allEnemies = [];
 
 let enemy1 = new Enemy();
 let enemy2 = new Enemy();
 let enemy3 = new Enemy();
-let enemy4 = new Enemy();
-let enemy5 = new Enemy();
-
-const allEnemies = [];
 
 allEnemies.push(enemy1);
 allEnemies.push(enemy2);
 allEnemies.push(enemy3);
-// allEnemies.push(enemy4);
-// allEnemies.push(enemy5);
-
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -43,20 +68,20 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
 	// all computers.
 	
-	this.offScreenX = this.x > 501;
-	if(this.offScreenX){
+	this.endOfRowX = this.x > 501;
+
+	if(this.endOfRowX) {
 		this.x = -100
 		this.y = randomRow();
-		this.speed = randomSpeed(2, 8);
-	}else{
+		this.speed = randomSpeed(2, 5);
+	}else {
 		this.x += this.speed + dt; 
 	}
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-	
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -71,8 +96,14 @@ let Player = function(x,y) {
 Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+	// all computers.
+	if(this.y === -10 || this.y > 400){
+		//win and reset position
+		playerReset();
+	}
+	checkCollisions();
 };
+
 
 // Draw the enemy on the screen, required method for game
 Player.prototype.render = function() {
@@ -97,26 +128,15 @@ Player.prototype.handleInput = function(keyCode) {
 				break;
 		}
 
+	// Keeps player sprite from going off the board
 	if(player.x < -2){
 		player.x = -2;
 	} else if(player.x > 402){
 		player.x = 402;
 	}
 
-	if(player.y === -10 || player.y > 400){
-		//win and reset position
-		player.x = 200;
-		player.y = 400;
-	}
-	// if x = 0 or 400 stop player from moving off the field
 	console.table(player);
 };
-
-// function randomPosition() {
-// 	const min = Math.ceil(1);
-// 	const max = Math.floor(3);
-// 	return Math.floor(Math.random() * (max - min)) + 1;
-// }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -127,7 +147,6 @@ let player = new Player(200,400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-	console.log(e.keyCode);
     var allowedKeys = {
         37: 'left',
         38: 'up',
